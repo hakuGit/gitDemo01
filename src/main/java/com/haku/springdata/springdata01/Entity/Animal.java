4d16c29396db9ca9099f10b1a7ebf3a4951bcb62
hey-rae <1171849689@qq.com>
2019-11-15 14:56:23 +0800

    zhm

---
+++ src/main/java/com/haku/springdata/springdata01/Entity/Animal.java
@@ -0,0 +1,14 @@
+package com.haku.springdata.springdata01.Entity;
+
+/**
+ * 功能描述:<br>
+ * 〈〉
+ *
+ * @author 11718
+ * @create 2019/11/15
+ * @since 1.0.0
+ */
+public class Animal {
+    private int id;
+    private String name;
+}
