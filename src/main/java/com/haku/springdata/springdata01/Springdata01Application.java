e62f8da32a80d4fa6e7ba111c274ea9853706d1b
admin <wugd@woniuxy.com>
2019-11-15 14:40:46 +0800

    sdad

--- src/main/java/com/haku/springdata/springdata01/Springdata01Application.java
+++ src/main/java/com/haku/springdata/springdata01/Springdata01Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Springdata01Application {
 
     public static void main(String[] args) {
+        ///sadadfdf
         SpringApplication.run(Springdata01Application.class, args);
     }
 
---
+++ src/main/java/com/haku/springdata/springdata01/dao/TesyDem.java
@@ -0,0 +1,12 @@
+package com.haku.springdata.springdata01.dao;
+
+/**
+ * 功能描述:<br>
+ * 〈〉
+ *
+ * @author wugd
+ * @create 2019/11/15
+ * @since 1.0.0
+ */
+public class TesyDem {
+}
